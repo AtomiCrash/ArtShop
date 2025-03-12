23ede464a4870382eade5725baca715529b87761
Maxim <375445379693@yandex.by>
2025-03-12 15:04:30 +0300

    version 1.01

--- src/main/java/com/example/artshop/ArtShopApplication.java
+++ src/main/java/com/example/artshop/ArtShopApplication.java
@@ -2,12 +2,10 @@ package com.example.artshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//http://localhost:8000/api/Art
+
 @SpringBootApplication
 public class ArtShopApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ArtShopApplication.class, args);
     }
-
 }
